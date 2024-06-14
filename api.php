e33f1f262bff5f21a9b1a57e31eb99cef508f313
Hossam <hossamsoliuman@gmail.com>
2024-06-14 17:51:14 +0300

    use post method

--- api.php
+++ api.php
@@ -2,15 +2,12 @@
 date_default_timezone_set('Asia/Riyadh');
 
 require 'db.php';
-$requestMethod = $_SERVER['REQUEST_METHOD'];
-$key = $requestMethod === 'POST' ? $_POST['key'] ?? null : $_GET['key'] ?? null;
 
-if ($requestMethod === 'POST' || $requestMethod === 'GET') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     require 'key.php';
-    if (isset($key) && $key == $apiKey) {
-        $gender = $requestMethod === 'POST' ? $_POST['gender'] ?? null : $_GET['gender'] ?? null;
-        $watched_at = $requestMethod === 'POST' ? ($_POST['watched_at'] ?? date('Y-m-d H:i:s')) : ($_GET['watched_at'] ?? date('Y-m-d H:i:s'));
-
+    if (isset($_POST['key']) && $_POST['key'] == $apiKey) {
+        $gender = $_POST['gender'];
+        $watched_at = isset($_POST['watched_at']) && !empty($_POST['watched_at']) ? $_POST['watched_at'] : date('Y-m-d H:i:s');
         if (in_array($gender, ['male', 'female', 'family'])) {
             $stmt = $db->prepare("INSERT INTO ad_watches (gender, watched_at) VALUES (:gender, :watched_at)");
             $stmt->bindValue(':gender', $gender, SQLITE3_TEXT);
@@ -26,4 +23,3 @@ if ($requestMethod === 'POST' || $requestMethod === 'GET') {
 } else {
     echo json_encode(['status' => 'error', 'message' => 'Invalid request method']);
 }
-?>
